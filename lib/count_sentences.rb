98da15bddd516982f5a99ef31685e8ee023d11b2
Janet Ngei <janet.ngei@student.moringaschool.com>
2022-08-17 22:19:27 +0300

    commit tree 8522378b2fa400c927d8f1268aefccec73f256bc

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,30 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with? "."
+
   end
 
   def question?
+    self.end_with? "?"
 
   end
 
   def exclamation?
+    self.end_with? "!"
 
   end
 
   def count_sentences
-
+    str = self.split(/[.!?]/)
+    str = str.delete_if {|elem| elem == ""}
+    no = str.count
+    #str = str.
+    #split_sent_array = self.split(/[.!?]/)
+    # pp split_sent_array
+    #split_sent_array.delete_if {|elem| elem == ""}
+    # pp split_sent_array
+    #no = split_sent_array.count
+    # binding.pry
   end
 end
