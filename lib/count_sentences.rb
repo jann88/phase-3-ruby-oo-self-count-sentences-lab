477093ead770489975db595481de5a0c2b0174f4
Janet Ngei <janet.ngei@student.moringaschool.com>
2022-08-17 22:21:08 +0300

    done

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,24 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with? "."
+
   end
 
   def question?
+    self.end_with? "?"
 
   end
 
   def exclamation?
+    self.end_with? "!"
 
   end
 
   def count_sentences
-
+    str = self.split(/[.!?]/)
+    str = str.delete_if {|elem| elem == ""}
+    no = str.count
+  #binding.pry
   end
 end
